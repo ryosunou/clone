492661e462dc402063d2cad8bf8f700300545e88
ryosunou <ryosunou@gmail.com>
2024-03-26 13:30:47 +0900

    ファイル追加

---
+++ src/php01/index06.php
@@ -0,0 +1,21 @@
+<?php
+peaple=array(
+    {
+        'last-name'=>'taro',
+        'age'=>25,
+        'sex'=>'men',
+    }
+    {
+        'last-name'=>'jiro',
+        'age'=>20,
+        'sex'=>'men',
+    }
+    {
+        'last-name'=>'hanako',
+        'age'=>16,
+        'sex'=>'women',
+    }
+    foreach($people as'last-name'=>$name.'age'=>$age.'sex'=>$sex){
+        echo 'last-name'."(".'$age'.'$sex'.")";
+    }
+)
